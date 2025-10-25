a1678b31439f97c6ce20c05ccbcb1738e8d996e5
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-10-24 22:06:04 -0700

    Update Configuration.py

--- src/Configuration.py
+++ src/Configuration.py
@@ -6,11 +6,15 @@ import orjson
 class configuration_for_users:
 
     def save_config(self, jsonfile):
-        #os.makedirs("UserConfigs", exist_ok=True)
-
-        #save_path = os.path.join("UserConfigs", "UserConfig.json")
         with open("UserConfigs.json", "wb") as f:
             f.write(orjson.dumps(jsonfile,option=orjson.OPT_INDENT_2))
+
+        if os.path.exists("UserConfigs.json"):
+            return True
+
+        if not os.path.exists("UserConfigs.json"):
+            return False
+
 
 
 
