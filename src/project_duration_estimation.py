dcf07beb4d976486b4257f68ae946f1fd94cecd2
Weebtrain <156955415+Weebtrain@users.noreply.github.com>
2025-11-01 21:35:09 -0700

    Documentation
    Added documentation for the project duration estimate code and tests.

--- src/project_duration_estimation.py
+++ src/project_duration_estimation.py
@@ -1,44 +1,75 @@
 import datetime
 
 class Project_Duration_Estimator:
+    '''
+    Class for estiamting the duration of a project after the files have had metadata extracted
+    Takes dictionary format of file hieracrhy on initialization and extracts "created" and "modified" dates from the files to create an estimate of project duration.
+
+    self.start_estimate stores the start date. Can be modified for future scenarios of user input.
+    self.end_estimate stores the end date. Can be modified for future scenarios of user input.
+    get/set functions are not designed for these yet.
+
+    get_duration() returns the project duration.
+    '''
+
 
     def __init__(self, hierarchy: dict):
-        self.hierarchy = hierarchy
+        '''
+        Takes in hierarchy of files with metadata upon initialization and pulls the datetime information neccessary
+        '''
+        self.hierarchy = hierarchy  #stores hierarchy for use
         self.__list_dates()
         self.__find_duration()
 
     def __list_dates(self):
-        self.created_dates = []
-        self.mod_dates = []
+        '''
+        Recursive method that traverses dictionary of hierarchy for creation and last modified datetimes
+        '''
+        self.created_dates = [] #list for all creation dates of files
+        self.mod_dates = [] #list for all last modified dates of files
 
-        self.__list_dates_recurse(self.hierarchy)
+        self.__list_dates_recurse(self.hierarchy)   #recursion function helper to this method
 
     def __list_dates_recurse(self, node: dict):
+        '''
+        Helper method to recursive function list_dates
+        Traverses hierarchy and sends files to have creation dates and last modified dates to be extracted
+        '''
         for file in node["children"]:
             if file["type"] != "DIR":
-                self.__add_file_dates(file)
+                self.__add_file_dates(file) #Recursively traverses files
             else:
-                self.__list_dates_recurse(file)
+                self.__list_dates_recurse(file) #extracts dates from files
 
     def __add_file_dates(self, file: dict):
+        '''
+        Function that takes a file from the recursive method list_dates_recursive and extracts the creation and last modified dates of files
+        '''
         self.created_dates.append(file["created"])
         self.mod_dates.append(file["modified"])
 
     def __find_duration(self):
-        start_estimate = self.created_dates[0]
-        end_estimate = self.mod_dates[0]
-        for date in self.created_dates:
+        '''
+        Takes a list of creation dates and last modified dates of files to determine an estimate of project length.
+        Also stores the earliest creation date and latest last modified date in case of user input changes.
+        '''
+        start_estimate = self.created_dates[0]  #Starter for earliest creation date
+        end_estimate = self.mod_dates[0]    #Starter for latest last modified date
+        for date in self.created_dates: #Finds earliest creation date
             if date < start_estimate:
                 start_estimate = date
 
-        for date in self.mod_dates:
+        for date in self.mod_dates: #Finds latest last modified date
             if date > end_estimate:
                 end_estimate = date
 
         self.start_estimate = start_estimate
         self.end_estimate = end_estimate
-        self.duration_estiamte = end_estimate - start_estimate
+        self.duration_estiamte = end_estimate - start_estimate  #Gives the estimated project duration in datetime.timedelta format (The result of taking a difference of datetimes)
 
     def get_duration(self) -> datetime.timedelta:
+        '''
+        Returns a datetime.timedelta showing the project duration estimate.
+        '''
         return self.duration_estiamte
 
--- test/test_project_duration.py
+++ test/test_project_duration.py
@@ -4,8 +4,11 @@ import datetime
 from src.project_duration_estimation import Project_Duration_Estimator
 
 class TestDurationEstimator(unittest.TestCase):
+    '''
+    Test class for project_duration_estimation.py, class Project_Duration_Estimator
+    '''
 
-    mock_dictionary = {
+    mock_dictionary = { #Dictionary mocking the format shown by data_extraction.py for testing purposes
         "type": "DIR",
         "children": [{
             "type": "FILE",
@@ -25,20 +28,35 @@ class TestDurationEstimator(unittest.TestCase):
         }]
     }
 
-    correct_end_date = datetime.datetime(2025, 11, 22) 
-    correct_start_date = datetime.datetime(2000, 1, 1)
-    correct_duration = correct_end_date - correct_start_date
+    correct_end_date = datetime.datetime(2025, 11, 22) #Correct latest last modified date of mock dictionary
+    correct_start_date = datetime.datetime(2000, 1, 1)  #Correct earliest creation date of mock dictionary
+    correct_duration = correct_end_date - correct_start_date    #Correct project duration of mock dictionary
 
     def setUp(self):
+        '''
+        Initalizes class for testing correct values. Inserts mock dictionary.
+        '''
         self.Duration_Estimator = Project_Duration_Estimator(self.mock_dictionary)
 
     def test_correct_end_date(self):
+        '''
+        Postitive Test.
+        Tests that the end estimate is correct for the mock dictionary
+        '''
         self.assertEqual(self.Duration_Estimator.end_estimate, self.correct_end_date)
 
     def test_correct_start_date(self):
+        '''
+        Positive Test.
+        Tests that the start estimate is correct for the mock dictionary
+        '''
         self.assertEqual(self.Duration_Estimator.start_estimate, self.correct_start_date)
 
     def test_correct_duration(self):
+        '''
+        Positive Test.
+        Tests that the duration estimate is correct for the mock dictionary.
+        '''
         self.assertEqual(self.Duration_Estimator.get_duration(), self.correct_duration)
 
 if __name__== "__main__":
