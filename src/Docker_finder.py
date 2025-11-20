7f46a039d3438fdb5e55fa09620579ab605de73c
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-11-19 22:36:49 -0800

    Update Docker_finder.py
    Now it gets the host ip and port number and returns them

--- src/Docker_finder.py
+++ src/Docker_finder.py
@@ -1,27 +1,33 @@
 
 import docker
-
+import socket
 class DockerFinder:
     def __init__(self):
         self.client = docker.from_env()
+        self.port_number=None
+        self.host_ip=None
+
+
 
 
     def get_mysql_host_port(self):
         try:
             con=None
-            port_number=None
             for container in self.client.containers.list():
 
                 if "database" in container.name:
                     con=self.client.containers.get(container.name)
             for key,value in con.ports.items():
                 if value is not None:
-                    port_number=value[0]['HostPort']
+                    self.port_number=value[0]['HostPort']
+                    self.host_ip=value[0]['HostIp']
+
+            if self.host_ip =="0.0.0.0":
+                self.host_ip="127.0.0.1" or "localhost"
 
 
-            return port_number
+            return self.port_number,self.host_ip
         except Exception as e:
             print(e)
-
 
 
