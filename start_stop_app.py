7ef62637526f6aba74ff679559730a1c822e30e2
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-11-21 12:44:36 -0800

    Update start_stop_app.py
    added docstrings for documentation

--- start_stop_app.py
+++ start_stop_app.py
@@ -7,18 +7,25 @@ class StartDockerApp:
         self.docker_compose_file_path = Path(__file__).resolve().parent 
         #getting the location of where the docker-compose file is and which folder it's
 
-    def run_app(self):
+
+    def setup_app(self):
         print(self.docker_compose_file_path)
         # step 1 we run   docker compose down -v
         print("\nStep 1: Stopping containers...")
         subprocess.run(['docker', 'compose', 'down', '-v'], cwd=self.docker_compose_file_path,
-                                           capture_output=True)
+                       capture_output=True)
 
         print("\nStep 2: Building images...")
-        #Here we are building the Docker containers 
-        subprocess.run(['docker', 'compose', 'build', '--no-cache'],
-                                           cwd=self.docker_compose_file_path, capture_output=True)
+        # Here we are building the Docker containers
+        building_process=subprocess.run(['docker', 'compose', 'buil                       cwd=self.docker_compose_file_path, capture_output=True)
+        if building_process.returncode == 0:
+ss.returncode == 0:
+            print("successfully built images")
+fully built images")
+            return
 
+
+    def run_app(self):
         print("\nStep 3: Starting services...")
         #Here we run the initialization and run the Docker containers 
         step_3_process = subprocess.run(['docker', 'compose', 'up', '-d'], cwd=self.docker_compose_file_path,
@@ -39,6 +46,13 @@ class StartDockerApp:
 
 
 if __name__ == "__main__":
+    """
+    Here we are running the docker-compose file
+    through the command line where the following commands
+    do the following
+    - start_stop_app start: Start the docker instance process
+    - start_stop_app stop: Stop the docker instance process
+    """
     app=StartDockerApp()
     if len(sys.argv)>1:
         if sys.argv[1]=="start":
