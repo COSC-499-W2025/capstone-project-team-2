806fd159fb71082f3bbbc2bfb317e99e0c2d70e0
Weebtrain <cmaranda2001@gmail.com>
2025-11-29 23:00:30 -0800

    Data gathering and scrubbing
    Following current ollama output dictionary format, gathering and scrubbing data performed.

--- src/ai_data_scrubbing.py
+++ src/ai_data_scrubbing.py
@@ -1,9 +1,93 @@
 import re
 
 class ai_data_scrubber:
+    '''
+    Class that conatins methods to gather and scrub data from ollama AI analysis of project files
+
+    Current output in format:
+    new_dict = {
+        "design_concepts": design_concepts,
+        "structures_used": data_structures,
+        "time_complexities_recorded": time_complexities_recorded,
+        "space_complexities_recorded": space_complexities_recorded,
+        "control_flow_and_error_handling_patterns": control_flow_and_error_handling_patterns,
+        "libraries_detected": libraries_detected,
+        "inferred_strengths": inferred_strengths
+    }
+    '''
     
-    def __init__(self, analysis: dict) -> dict:
-        pass
+    def __init__(self, analysis: dict):
+        '''
+        Constructor
+        Immediately takes unprocessed ollama AI analysis and gathers and scrubs useful data from it
+
+        Use get_scrubbed_dict to return final dictionary
+        '''
+        design_concepts = self._gather_from_list(self._gather_from_dict(analysis, "design_and_architecture"), "concepts_observed")  #Gathers "design_and_architecture" dictionaries then pulls "concepts_observed" that is nested in those dictionaries
+        design_concepts = self._remove_sentence_data(design_concepts, tolerance=3)  #Removes possible sentences, analysis of a prior output shows that 3 spaces are possible in good data
+        design_concepts = self._gather_unique(design_concepts)  #Removes extra values to ensure all values unique
+        design_concepts = self._remove_similar_data(design_concepts)    #Removes possible iterations that contain differing special characters
+
+        #Gather dictionaries for contained keys
+        data_structures_and_algorithms = self._gather_from_dict(analysis, "data_structures_and_algorithms")
+
+        data_structures = self._gather_from_list(data_structures_and_algorithms, "structures_used")
+        data_structures = self._remove_sentence_data(data_structures, tolerance=1)  #Tolerance 1 rationlized by looking at a generated analysis
+        data_structures = self._gather_unique(data_structures)
+        #Unsure if this scrub will be needed so it's commented out for now
+        #data_structures = self._remove_similar_data(data_structures, similarity_word_num=1) #Considering data structure names, one similar word should be valid
+
+        time_complexities_recorded = self._gather_from_list(data_structures_and_algorithms, "time_complexity")
+        time_complexities_recorded = self._gather_from_list(time_complexities_recorded, "average_case") #Average case used for recorded time complexities. Can add all three later if wanted.
+        time_complexities_recorded = self._clip_sentences_off_data(time_complexities_recorded, character=')', clip_after_character=True)    #This use of character ')' should only clip after a big O notation ends
+        time_complexities_recorded = self._remove_sentence_data(time_complexities_recorded, tolerance=2)    #nlogn was seen as the max number of spaces at 2, anything more should be sentences with no notation or notation placed in the middle
+        time_complexities_recorded = self._gather_unique(time_complexities_recorded, casing="") #We don't want to change the casing to preserve correct big O notation
+
+        space_complexities_recorded = self._gather_from_list(data_structures_and_algorithms, "space_complexity")
+        space_complexities_recorded = self._clip_sentences_off_data(space_complexities_recorded, character=')', clip_after_character=True)    #This use of character ')' should only clip after a big O notation ends
+        space_complexities_recorded = self._remove_sentence_data(space_complexities_recorded, tolerance=2)    #nlogn was seen as the max number of spaces at 2, anything more should be sentences with no notation or notation placed in the middle
+        space_complexities_recorded = self._gather_unique(space_complexities_recorded, casing="") #We don't want to change the casing to preserve correct big O notation
+
+        #Gather dictionaries for contained keys
+        control_flow_and_error_handling = self._gather_from_dict(analysis, "control_flow_and_error_handling")
+
+        control_flow_and_error_handling_patterns = self._gather_from_list(control_flow_and_error_handling, "patterns")
+        control_flow_and_error_handling_patterns = self._remove_sentence_data(control_flow_and_error_handling_patterns, tolerance=5)    #Tolerance assumed from data, larger sections more helpful here than other data values
+        control_flow_and_error_handling_patterns = self._gather_unique(control_flow_and_error_handling_patterns)
+        control_flow_and_error_handling_patterns = self._remove_similar_data(control_flow_and_error_handling_patterns)  #2 word similarity should work best, though may be unstable with larger strings if larger strings found first
+
+        #error handling quality to be added later, requires averaging string ratings for overall output since list of ratings isn't helpful unless associated with files which isn't supported yet and some outputs aren't simple one word responses
+
+        #Gather dictionaries for contained keys
+        library_and_framework_usage = self._gather_from_dict(analysis, "library_and_framework_usage")
+
+        libraries_detected = self._gather_from_list(library_and_framework_usage, "libraries_detected")
+        libraries_detected = self._remove_sentence_data(libraries_detected) #No spaces should be found in libraries
+        libraries_detected = self._gather_unique(libraries_detected)
+
+        #Not adding any of these yet since data would require file association or averaging of strings. Useful data however.
+        #code_quality_and_maintainability
+        #readability
+        #testability
+        #technical debt
+
+        #inferred_strengths
+        inferred_strengths = self._gather_from_dict(analysis, "inferred_strengths")
+        inferred_strengths = self._remove_sentence_data(inferred_strengths, tolerance=4)    #Strings longer than 5 words stop being meaningful strenghts and start being observations of uses
+        inferred_strengths = self._gather_unique(inferred_strengths)
+
+        #Growth areas discluded for now considering it is mostly sentence data but theoretically useful data for future releases
+
+        new_dict = {
+            "design_concepts": design_concepts,
+            "structures_used": data_structures,
+            "time_complexities_recorded": time_complexities_recorded,
+            "space_complexities_recorded": space_complexities_recorded,
+            "control_flow_and_error_handling_patterns": control_flow_and_error_handling_patterns,
+            "libraries_detected": libraries_detected,
+            "inferred_strengths": inferred_strengths
+        }
+        self.new_dict = new_dict
 
     def _remove_sentence_data(self, data: list, tolerance=0) -> list:
         '''
@@ -160,3 +244,31 @@ class ai_data_scrubber:
                 else:
                     value_list.append(value[key])
         return value_list
+    
+    def _gather_from_list(self, dict_list: list, key: str) -> list:
+        '''
+        Iterates over every gathered dictionary in list when wanted key is nested in a second dictionary of AI analysis.
+
+        paramters:
+            dict_list: list of dictionaries from _gather_from_dict when wanted key is nested in a second dictionary in every analysis
+            key: dictionary index/key to gather data into list from
+
+        return:
+            list of all data from key value. Not unique.
+        '''
+        value_list = list()
+        for dictionary in dict_list:
+            if key in dictionary:
+                if type(dictionary[key]) is list:    #If value of dictionary[key] is a list, we add all elements of list to value_list instead of a list as one element
+                    value_list.extend(dictionary[key])
+                else:
+                    value_list.append(dictionary[key])
+        return value_list
+
+    def get_scrubbed_dict(self) -> dict:
+        '''
+        Returns gathered and scrubbed data
+
+        Refer to class for current output dictionary format
+        '''
+        return self.new_dict
