305baa99bdd71b1d542051d676da7c6b2c54d702
ThunderIW <im.wiessler@gmail.com>
2026-01-01 02:53:29 -0800

    Rename Generate_Render_CV_Portfolio to Generate_RenderCV_Resume

--- test/test_Generate_Render_CV_Portfolio.py
+++ test/test_Generate_Render_CV_Portfolio.py
@@ -7,7 +7,7 @@ import tempfile
 import shutil
 
 # Add src directory to path
-from src.reporting.Generate_Render_CV_Portfolio import (
+from src.reporting.Generate_RenderCV_Resume import (
     Education,
     Connections,
     Project,
