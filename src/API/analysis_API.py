65caa13e802fe408fc600e917746872ee362ac44
Weebtrain <cmaranda2001@gmail.com>
2026-01-09 13:48:06 -0800

    Refactor zip extraction
    Finally changed the zip extraction to extract to a file that shares a name with the zip. Added base for API building in process.

---
+++ src/API/analysis_API.py
@@ -0,0 +1,14 @@
+from src.core.analysis_service import (
+    input_path,
+    analyze_project,
+    extract_if_zip,
+)
+
+from pathlib import Path
+import json
+import os
+
+from src.core.app_context import AppContext
+
+def perform_analysis_API(ctx: AppContext, path: Path):
+    pass
