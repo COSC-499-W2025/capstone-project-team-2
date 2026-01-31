0b17c4e51e3d019e1b76528b1fbda6cb01f251d2
Graves067 <c.a.gillespie9@gmail.com>
2026-01-31 02:39:52 -0800

    added project history table

--- database.sql
+++ database.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS appdb;
 
 USE appdb;
 
+-- primary project--
 CREATE TABLE IF NOT EXISTS project_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
@@ -14,8 +15,15 @@ CREATE TABLE IF NOT EXISTS project_data (
     INDEX idx_filename (filename)
 ) ENGINE=InnoDB;
 
-
+-- project history table--
 CREATE TABLE IF NOT EXISTS project_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id INT NOT NULL,
+    version_number INT NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    content JSON NOT NULL,
+    file_blob LONGBLOB,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 );
 
