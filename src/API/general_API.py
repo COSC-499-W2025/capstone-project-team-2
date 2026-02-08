a93f3743204706976c3bf05dd5a12d136ea89914
Puneet-Maan <8puneet8@gmail.com>
2026-02-08 11:27:58 -0800

    add user controlled representation prefs (API and CLI) with tests

--- src/API/general_API.py
+++ src/API/general_API.py
@@ -4,6 +4,7 @@ from .consent_API import consentRouter
 from .skills_API import skillsRouter
 from .project_io_API import projectsRouter
 from .Portfolio_Generator_API import portfolioRouter
+from .representation_API import representationRouter
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ app.include_router(consentRouter)
 app.include_router(projectsRouter)
 app.include_router(skillsRouter)
 app.include_router(portfolioRouter)
+app.include_router(representationRouter)
